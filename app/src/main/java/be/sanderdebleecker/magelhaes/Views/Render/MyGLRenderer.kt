3d10d2c75889d5b90808ed0b7db6144d823d6729
simulan <simulan88@yahoo.com>
2019-02-25 21:40:57 +0100

    openGL rotationmatrix phase

--- app/src/main/java/be/sanderdebleecker/magelhaes/Views/Render/MyGLRenderer.kt
+++ app/src/main/java/be/sanderdebleecker/magelhaes/Views/Render/MyGLRenderer.kt
@@ -12,18 +12,35 @@ class MyGLRenderer : GLSurfaceView.Renderer {
     private val vPMatrix = FloatArray(16)
     private val projectionMatrix = FloatArray(16)
     private val viewMatrix = FloatArray(16)
+    private val rotationMatrix = FloatArray(16)
 
     override fun onSurfaceCreated(unused: GL10?, config: EGLConfig) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
         mTriangle = Triangle()
     }
     override fun onDrawFrame(unused: GL10) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+
+        val resultingMatrix = FloatArray(16)
+
         // Set the camera position (View matrix)
         Matrix.setLookAtM(viewMatrix, 0, 0f, 0f, -3f, 0f, 0f, 0f, 0f, 1.0f, 0.0f)
 
         // Calculate the projection and view transformation
         Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
 
-        mTriangle.draw(vPMatrix)
+        // Create a rotation transformation for the triangle
+        val time = System.currentTimeMillis() % 4000L
+        val angle = 0.090f * time.toInt()
+        Matrix.setRotateM(rotationMatrix, 0, angle, 0f, 0f, -1.0f)
+
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the vPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        Matrix.multiplyMM(resultingMatrix, 0, vPMatrix, 0, rotationMatrix, 0)
+
+
+        mTriangle.draw(resultingMatrix)
     }
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
